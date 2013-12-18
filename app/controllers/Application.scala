0203c744ed59d954ad2b8be7add7629fbe67cadc
simao <lasering@gmai.com>
2013-12-18 02:17:04 +0000

    Further integration of Play Forms and Bootstrap forms. Also included implicit internationalization for the forms.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,69 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-//import models._
-import views._
 import play.api.data._
 import play.api.data.Forms._
+//import models._
+import views._
+
 import java.util.Date
 
 object Application extends Controller {
 
   def index = Action {
     Redirect(routes.Application.hackers)
-  }
-
-  //Register
-  def registerHackerspace = Action {
-    Ok(html.register.hackerspace())
-  }
-  def newHackerspace = Action {
-    Ok
-  }
-  
-  case class Hacker(
-      username: String,
-      name: String,
-      password: String,
-      email: String,
-      birthday: Date,
-      sex: String
-  )
-  
-  val hackerForm: Form[Hacker] = Form(
-	mapping(
-	  "username" -> nonEmptyText,
-	  "name" -> nonEmptyText,
-	  "password" -> tuple(
-        "main" -> nonEmptyText(minLength = 6),
-        "confirm" -> text(minLength = 6)
-      ).verifying("Passwords don't match", passwords => passwords._1 == passwords._2),
-	  "email" -> email,
-	  "birthday" -> date,
-	  "sex" -> text
-    ){
-      // Binding: Create a Hacker from the mapping result (ignore the second password)
-      (username, name, passwords, email, birthday, sex) => Hacker(username, name, passwords._1, email, birthday, sex) 
-    }{
-      // Unbinding: Create the mapping values from an existing Hacker value
-      Hacker => Some(Hacker.username, Hacker.name, (Hacker.password, Hacker.password), Hacker.email, Hacker.birthday, Hacker.sex)
-    }
-  )
-  
-  def registerHacker = Action {
-    Ok(html.register.hacker(hackerForm))
-  }
-  def newHacker = Action {implicit request =>
-    hackerForm.bindFromRequest.fold(
-      // Form has errors, redisplay it
-      errors => BadRequest(html.register.hacker(errors)),
-      
-      // We got a valid Hacker value, display the summary
-      hacker => Ok(html.register.index())
-    )
-  }
-  def register = Action {
-    Ok(html.register.index())
   }
   
   //Hackers
---
+++ app/controllers/Register.scala
@@ -0,0 +1,68 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+//import models._
+import views._
+
+import java.util.Date
+
+object Register extends Controller {
+  //Index
+  def registerIndex = Action {
+    Ok(html.register.index())
+  }
+  
+  //Hackerspace
+  def hackerspace = Action {
+    Ok(html.register.hackerspace())
+  }
+  def newHackerspace = Action {
+    Ok
+  }
+  
+  //Hacker
+  case class HackerDTO(
+      username: String,
+      name: String,
+      password: String,
+      email: String,
+      birthday: Date,
+      sex: String
+  )
+  
+  val hackerForm: Form[HackerDTO] = Form(
+	mapping(
+	  "username" -> nonEmptyText,
+	  "name" -> nonEmptyText,
+	  "password" -> tuple(
+        "main" -> nonEmptyText(minLength = 6),
+        "confirm" -> nonEmptyText(minLength = 6)
+      ).verifying("Passwords don't match", passwords => passwords._1 == passwords._2),
+	  "email" -> email,
+	  "birthday" -> date("dd-mm-yyyy"),
+	  "sex" -> nonEmptyText
+    ){
+      // Binding: Create a Hacker from the mapping result (ignore the second password)
+      (username, name, passwords, email, birthday, sex) => HackerDTO(username, name, passwords._1, email, birthday, sex) 
+    }{
+      // Unbinding: Create the mapping values from an existing Hacker value
+      hackerDTO => Some(hackerDTO.username, hackerDTO.name, (hackerDTO.password, hackerDTO.password), hackerDTO.email, hackerDTO.birthday, hackerDTO.sex)
+    }
+  )
+  
+  def hacker = Action {
+    Ok(html.register.hacker(hackerForm))
+  }
+  def newHacker = Action {implicit request =>
+    hackerForm.bindFromRequest.fold(
+      // Form has errors, redisplay it
+      errors => BadRequest(html.register.hacker(errors)),
+      
+      // We got a valid Hacker value, display the summary
+      hacker => Ok(html.register.index())
+    )
+  }
+}
