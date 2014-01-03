433e1c0cfbd6eb8b647c1114b7cfd2b90a378692
Simão Martins <lasering@gmail.com>
2014-01-03 18:47:19 +0000

    Created new hackerspace form.

--- app/controllers/Register.scala
+++ app/controllers/Register.scala
@@ -18,11 +18,48 @@ object Register extends Controller {
   }
   
   //Hackerspace
+  case class HackerspaceDTO(
+      username: String,
+      name: String,
+      password: String,
+      email: String,
+      location: String,
+      GPS: String,
+      dateFounded: Date
+  )
+  
+  val hackerspaceForm: Form[HackerspaceDTO] = Form(
+	mapping(
+	  "username" -> nonEmptyText,
+	  "name" -> nonEmptyText,
+	  "password" -> tuple(
+        "main" -> nonEmptyText(minLength = 6),
+        "confirm" -> nonEmptyText(minLength = 6)
+      ).verifying("Passwords don't match", passwords => passwords._1 == passwords._2),
+	  "email" -> email.verifying(nonEmpty),
+	  "location" -> nonEmptyText,
+	  "GPS" -> nonEmptyText,
+	  "dateFounded" -> date("dd-mm-yyyy")
+    ){
+      // Binding: Create a Hacker from the mapping result (ignore the second password)
+      (username, name, passwords, email, location, GPS, dateFounded) => HackerspaceDTO(username, name, passwords._1, email, location, GPS, dateFounded) 
+    }{
+      // Unbinding: Create the mapping values from an existing Hacker value
+      hackerspaceDTO => Some(hackerspaceDTO.username, hackerspaceDTO.name, (hackerspaceDTO.password, hackerspaceDTO.password), hackerspaceDTO.email, hackerspaceDTO.location, hackerspaceDTO.GPS, hackerspaceDTO.dateFounded)
+    }
+  )
+  
   def hackerspace = Action {
-    Ok(html.register.hackerspace())
+    Ok(html.register.hackerspace(hackerspaceForm))
   }
-  def newHackerspace = Action {
-    Ok
+  def newHackerspace = Action {implicit request =>
+    hackerspaceForm.bindFromRequest.fold(
+      // Form has errors, redisplay it
+      errors => BadRequest(html.register.hackerspace(errors)),
+      
+      // We got a valid Hacker value, display the summary
+      hackerspaceDTO => Ok(html.register.index())
+    )
   }
   
   //Hacker
