f03ab6ca71999ab993726748560bf4bc1fafa6b0
diutsu <sousago@gmail.com>
2014-02-16 17:02:57 +0000

    dml changes

---
+++ app/models/GPS.java
@@ -0,0 +1,13 @@
+package models;
+import java.io.Serializable;
+public class GPS implements Serializable{
+    
+    String latitude;
+    String longitude;
+
+    public GPS(Object obj) {
+    }
+    public GPS() {
+    }
+    
+}
