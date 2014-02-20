d908ea0ae57fcef0328e61ef5bdecf1013fce5a9
Simão Martins <lasering@gmail.com>
2014-02-20 01:59:16 +0000

    Revert "Removed some classes that should have not been commited."
    This reverts commit 7d475e85a518961ad05e15f46dc9e9e31c0da7b2.

---
+++ app/models/core/Externalization.java
@@ -0,0 +1,11 @@
+package models.core;
+import java.security.KeyPair;
+
+public class Externalization {
+    public static byte[] externalizeKeyPair(KeyPair kp) {
+            return null;
+    }
+    public static <T> T internalizeKeyPair(byte[] bytearray) {
+        return null;
+    }
+}
