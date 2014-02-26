8e5f7835a5b41828a3db00829c8ca821f0497c1b
David Duarte <david.duarte@ist.utl.pt>
2014-02-26 13:39:45 +0000

    First version of login service

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -1,31 +1,28 @@
 package controllers
 
-import play.api._
 import play.api.data._
 import play.api.data.Forms._
-import play.api.data.validation.Constraints._
 import play.api.mvc._
 import views._
+import services.dto._
 
 object Login extends Controller {
   
-  case class UserDTO(username: String, password: String)
-  
-  val loginForm: Form[UserDTO] = Form(
+  val loginForm: Form[UserLoginDTO] = Form(
   	mapping (
   		"username" -> nonEmptyText,
   		"password" -> nonEmptyText(minLength = 6)
-  	) (UserDTO.apply)(UserDTO.unapply)
+  	) (UserLoginDTO.apply)(UserLoginDTO.unapply)
   )
 
   def index() = Action  {
-	Ok(html.login.index(loginForm))
+	  Ok(html.login.index(loginForm))
   }
   
   def submit = Action {implicit request => 
-	loginForm.bindFromRequest.fold(
-		errors => BadRequest(html.login.index(errors)),
-		UserDTO => Redirect(routes.Application.index)
-	)
+    loginForm.bindFromRequest.fold(
+      errors => BadRequest(html.login.index(errors)),
+      UserDTO => Redirect(routes.Application.index)
+    )
   }
 }
--- app/models/User.scala
+++ app/models/User.scala
@@ -1,4 +1,33 @@
 package models
 
-class User extends User_Base {
+import models.core.security.PasswordEncryption
+
+class User(var username: String, var registryEmail: String, val password: String) extends User_Base {
+
+  this.setUsername(username)
+  this.setRegistryEmail(registryEmail)
+  this.password_=(password)
+  this.salt()
+  
+  def password_=(password: String) = {
+    setPassword(PasswordEncryption.getEncryptedPassword(password, getSalt()))
+  }
+  
+  def salt() = {
+    if (getSalt().isEmpty) {
+      setSalt(PasswordEncryption.generateSalt())
+    }
+  }
+
+  override protected def setPassword(password: Array[Byte]): Unit = {
+    super.setPassword(password)
+  }
+  
+  override protected def setSalt(salt: Array[Byte]): Unit = {
+    super.setSalt(salt)
+  }
+  
+  def authenticate(password: String): Boolean = {
+    PasswordEncryption.authenticate(password, getPassword(), getSalt());
+  }
 }
